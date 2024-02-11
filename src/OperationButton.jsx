50060f1251600839721201c3e8ef64bc5fe8642b
Jason Mueller <jasonmueller17@yahoo.ca>
2024-02-10 20:43:24 -0500

    init

---
+++ src/OperationButton.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { ACTIONS } from "./App";
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
